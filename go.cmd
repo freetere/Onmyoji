c2cc96567bb52c0e2a33c73e8bbc84afebecbf15
freetere <599425708@qq.com>
2021-09-27 16:38:00 +0800

    更新五周年活动,新增更为简介的运行方式go.cmd

--- go.cmd
+++ go.cmd
@@ -1,5 +1,4 @@
 @echo off
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd /d "%~dp0"
-
-start python main.py
+start python.exe main.py
